d1c4c7b0e7dc6fb31003f2d65e4ee3eda9e2d7b7
Naman <naman0105@gmail.com>
2017-11-19 22:28:20 +0530

    first commit

---
+++ app/src/main/java/com/example/naman/fun_trip/AddTripActivity.java
@@ -0,0 +1,19 @@
+package com.example.naman.fun_trip;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by naman on 19/11/17.
+ */
+
+public class AddTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_trip_activity);
+    }
+
+
+}
