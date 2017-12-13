2e70654b19c97fbf03a58027a60a730bf175c014
naman <naman0105@gmail.com>
2017-12-13 23:55:05 +0530

    naman

--- app/src/main/java/com/example/naman/fun_trip/UserRegistration.java
+++ app/src/main/java/com/example/naman/fun_trip/UserRegistration.java
@@ -2,6 +2,9 @@ package com.example.naman.fun_trip;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -14,7 +17,9 @@ public class UserRegistration extends AppCompatActivity {
 
 
     private DatabaseReference mDatabase;
-
+    private Button submitButton;
+    private EditText et_name;
+    private String name,email,password,confirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 // ...
@@ -24,8 +29,20 @@ public class UserRegistration extends AppCompatActivity {
 
         Bundle bundle = new Bundle();
         bundle  = getIntent().getExtras();
-        String phNumber = bundle.getString("PhNumber");
+        final String phNumber = bundle.getString("PhNumber");
         System.out.println("In user registration class "+ phNumber);
         mDatabase.child("users").setValue(phNumber);
+
+        submitButton = (Button) findViewById(R.id.submit_button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et_name = (EditText)findViewById(R.id.name);
+                System.out.println(et_name);
+                mDatabase.child("users").child(phNumber).setValue("Name",et_name);
+            }
+        });
     }
+
 }
